29b61409f8e65accd0713e8ca829e1dc746b2780
faisal-saddique <faisalsaddique7964@gmail.com>
2023-09-24 11:41:47 +0500

    made fixes to the app

--- app.py
+++ app.py
@@ -22,8 +22,11 @@ load_dotenv()
 st.set_page_config(page_title="FireBot", page_icon="🔥🚒")
 st.title("🔥🚒 FireBot")
 
+
 @st.cache_resource(ttl="1h")
 def configure_retriever():
+    pinecone.init(api_key=os.getenv("PINECONE_API_KEY"),
+              environment=os.getenv("PINECONE_ENVIRONMENT"))
     # Create OpenAIEmbeddings object using the provided API key
     embeddings = OpenAIEmbeddings()
     docsearch = Pinecone.from_existing_index(index_name=os.getenv("PINECONE_INDEX"),embedding=embeddings)
@@ -60,7 +63,7 @@ class PrintRetrievalHandler(BaseCallbackHandler):
 
     def on_retriever_end(self, documents, **kwargs):
         for idx, doc in enumerate(documents):
-            source = os.path.basename(doc.metadata["source"])
+            source = os.path.basename(doc.metadata["path"])
             self.status.write(f"**Document {idx} from {source}**")
             self.status.markdown(doc.page_content)
         self.status.update(state="complete")
@@ -70,15 +73,15 @@ retriever = configure_retriever()
 
 # Setup memory for contextual conversation
 msgs = StreamlitChatMessageHistory()
-memory = ConversationBufferMemory(memory_key="chat_history", chat_memory=msgs, return_messages=True)
-
+memory = ConversationBufferMemory(memory_key="chat_history", chat_memory=msgs, return_messages=True, input_key='question', output_key='answer')
+# memory = ConversationSummaryBufferMemory(llm=llm, input_key='question', output_key='answer')
 # Setup LLM and QA chain
 llm = ChatOpenAI(
     model_name="gpt-3.5-turbo", temperature=0, streaming=True
 )
 
 qa_chain = ConversationalRetrievalChain.from_llm(
-    llm, retriever=retriever, memory=memory, verbose=True
+    llm, retriever=retriever, memory=memory, verbose=True,return_source_documents=True
 )
 
 if len(msgs.messages) == 0 or st.sidebar.button("Clear message history"):
@@ -95,4 +98,5 @@ if user_query := st.chat_input(placeholder="Ask me anything!"):
     with st.chat_message("assistant"):
         retrieval_handler = PrintRetrievalHandler(st.container())
         stream_handler = StreamHandler(st.empty())
-        response = qa_chain.run(user_query, callbacks=[retrieval_handler, stream_handler])
+        response = qa_chain(user_query, callbacks=[retrieval_handler, stream_handler])
+        st.success(response)
